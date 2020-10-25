ba99341fc06fd0564d22ff45b15838c154118e64
B7-9414 <banawi.bassam@gmail.com>
2020-10-25 18:33:22 -0400

    micro-front

---
+++ app3/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { assetUrl } from 'src/single-spa/asset-url';
+
+@Component({
+  selector: 'app3-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app3';
+  yoshiUrl = assetUrl("yoshi.png");
+}
